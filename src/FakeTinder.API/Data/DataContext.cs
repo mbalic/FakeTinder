45b87626d4e4cb7dca0f220af8ecf212275d610a
Mario Balić <mario.balic1@gmail.com>
2019-08-20 19:54:14 +0200

    initial commit

---
+++ src/FakeTinder.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using FakeTinder.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeTinder.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ src/FakeTinder.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace FakeTinder.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
