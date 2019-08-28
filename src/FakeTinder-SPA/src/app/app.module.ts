ae50e45000f4c6e2e7bf54c5b035ac4d229d9940
Mario Balić <mario.balic1@gmail.com>
2019-08-28 19:33:35 +0200

    client side login

--- src/FakeTinder-SPA/src/app/app.module.ts
+++ src/FakeTinder-SPA/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_service/auth.service';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
