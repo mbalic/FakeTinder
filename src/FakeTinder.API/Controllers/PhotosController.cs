a95ae6b47a16bc95460a3a91f6ec5a886fb6c94c
Mario Balić <mario.balic1@gmail.com>
2019-09-24 13:13:33 +0200

    SetMainPhoto API

--- src/FakeTinder.API/Controllers/PhotosController.cs
+++ src/FakeTinder.API/Controllers/PhotosController.cs
@@ -94,5 +94,39 @@ namespace FakeTinder.API.Controllers
 
             return BadRequest("Could not add photo.");
         }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var userFromRepo = await this._repo.GetUser(userId);
+
+            if (!userFromRepo.Photos.Any(p => p.Id == id))
+            {
+                return Unauthorized();
+            }
+
+            var photoFromRepo = await this._repo.GetPhoto(id);
+
+            if (photoFromRepo.IsMain)
+            {
+                return BadRequest("This is already the main photo.");
+            }
+
+            var currentMainPhoto = await this._repo.GetMainPhotoForUser(userId);
+            currentMainPhoto.IsMain = false;
+            photoFromRepo.IsMain = true;
+
+            if (await this._repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Could not set photo to main.");
+        }
     }
 }
--- src/FakeTinder.API/Data/DatingRepository.cs
+++ src/FakeTinder.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FakeTinder.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,13 @@ namespace FakeTinder.API.Data
         public void Delete<T>(T entity) where T : class
         {
             this._context.Remove(entity);
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await this._context.Photos
+                .Where(p => p.UserId == userId)
+                .FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
