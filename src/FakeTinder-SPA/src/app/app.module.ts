16ceb14d6726c3f2359a3950678687398647d90e
Mario Balić <mario.balic1@gmail.com>
2019-10-01 19:37:20 +0200

    user pagination on front

--- src/FakeTinder-SPA/src/app/app.module.ts
+++ src/FakeTinder-SPA/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   BsDropdownModule,
   TabsModule,
-  BsDatepickerModule
+  BsDatepickerModule,
+  PaginationModule
 } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
@@ -63,6 +64,7 @@ export function tokenGetter() {
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
