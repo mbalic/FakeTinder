215035e1965ce9a797d49b263022e5a2722a4a1c
Mario Balić <mario.balic1@gmail.com>
2019-10-01 21:58:02 +0200

    adding filters on members list

--- src/FakeTinder-SPA/src/app/members/member-list/member-list.component.ts
+++ src/FakeTinder-SPA/src/app/members/member-list/member-list.component.ts
@@ -12,6 +12,12 @@ import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList: [
+    { value: 'male'; display: 'Males' },
+    { value: 'female'; display: 'Females' }
+  ];
+  userParams: any = {};
   pagination: Pagination;
 
   constructor(
@@ -25,6 +31,11 @@ export class MemberListComponent implements OnInit {
       this.users = data['users'].result;
       this.pagination = data['users'].pagination;
     });
+
+    this.userParams.genderList =
+      this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
   }
 
   pageChanged(event: any): void {
@@ -32,9 +43,21 @@ export class MemberListComponent implements OnInit {
     this.loadUsers();
   }
 
+  resetFilters() {
+    this.userParams.genderList =
+      this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
   loadUsers() {
     this.userService
-      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .getUsers(
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage,
+        this.userParams
+      )
       .subscribe(
         (res: PaginatedResult<User[]>) => {
           this.users = res.result;
