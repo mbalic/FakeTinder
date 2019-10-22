7a1476b95fe19e93a68a5330a83002ba4498cac2
Mario Balić <mario.balic1@gmail.com>
2019-10-22 17:10:59 +0200

    adding roles to seed

--- src/FakeTinder.API/Data/Seed.cs
+++ src/FakeTinder.API/Data/Seed.cs
@@ -10,9 +10,11 @@ namespace FakeTinder.API.Data
     public class Seed
     {
         private readonly UserManager<User> _userManager;
-        public Seed(UserManager<User> userManager)
+        private readonly RoleManager<Role> _roleManager;
+        public Seed(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             this._userManager = userManager;
+            this._roleManager = roleManager;
         }
 
         public void SeedUsers()
@@ -21,10 +23,36 @@ namespace FakeTinder.API.Data
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                var roles = new List<Role>
+                {
+                    new Role{Name = "Member"},
+                    new Role{Name = "Admin"},
+                    new Role{Name = "Moderator"},
+                    new Role{Name = "VIP"}
+                };
+
+                foreach (var role in roles)
+                {
+                    this._roleManager.CreateAsync(role).Wait();
+                }
 
                 foreach (var user in users)
                 {
                     this._userManager.CreateAsync(user, "password").Wait();
+                    this._userManager.AddToRoleAsync(user, "Member").Wait();
+                }
+
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                var result = this._userManager.CreateAsync(adminUser, "password").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = this._userManager.FindByNameAsync("Admin").Result;
+                    this._userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" }).Wait();
                 }
             }
         }
