7b57dc5e19854a672de107512e1869646847d750
Mario Balić <mario.balic1@gmail.com>
2019-11-07 19:29:50 +0100

    e2e tests

--- src/FakeTinder-SPA/e2e/src/app.e2e-spec.ts
+++ src/FakeTinder-SPA/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,12 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('Login', () => {
   let page: AppPage;
+
+  const wrongCredentias = {
+    username: 'wrongname',
+    password: 'wrongpasswd'
+  };
 
   beforeEach(() => {
     page = new AppPage();
@@ -9,6 +14,23 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to FakeTinder-SPA!');
+    expect(page.getParagraphText()).toEqual('Find your match');
+  });
+
+  it('when user trying to login with wrong credentials he should stay on “login” page', () => {
+    page.navigateTo();
+    page.fillCredentials(wrongCredentias);
+    expect(page.getParagraphText()).toEqual('Find your match');
+  });
+
+  it('when user trying to login with right credentials he should not stay on “login” page', () => {
+    page.navigateTo();
+    page.fillCredentials();
+    expect(page.getMatchesLink()).toEqual('Matches');
+  });
+
+  it('when user logs out he should go on “login” page', () => {
+    page.logOut();
+    expect(page.getParagraphText()).toEqual('Find your match');
   });
 });
--- src/FakeTinder-SPA/e2e/src/app.po.ts
+++ src/FakeTinder-SPA/e2e/src/app.po.ts
@@ -1,6 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  private credentials = {
+    username: 'admin',
+    password: 'password'
+  };
+
   navigateTo() {
     return browser.get('/');
   }
@@ -8,4 +13,20 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getMatchesLink() {
+    return element(by.css('app-root a#matches-link')).getText();
+  }
+
+  fillCredentials(credentials: any = this.credentials) {
+    element(by.css('[name="username"]')).sendKeys(credentials.username);
+    element(by.css('[name="password"]')).sendKeys(credentials.password);
+    element(by.css('.btn-success')).click();
+  }
+
+  logOut() {
+    element(by.css('#dropdown-menu')).click();
+    element(by.css('#logout')).click();
+  }
+
 }
